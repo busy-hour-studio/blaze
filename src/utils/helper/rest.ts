29997fec847cdb3a223e046b84c28cb6df619abe
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-02-02 19:45:30 +0700

    chore: organize imports

--- src/utils/helper/rest.ts
+++ src/utils/helper/rest.ts
@@ -1,5 +1,5 @@
-import { Hono } from 'hono';
 import { Method, RestParam, RestRoute } from '@/types/rest';
+import { Hono } from 'hono';
 
 export function extractRestPath(restRoute: RestRoute) {
   const restPath = restRoute.split(' ');
