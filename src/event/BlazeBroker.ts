2cc8764d80be4ff58c95e5856895d3aaf4c62201
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-02-04 18:25:17 +0700

    revert: broker call args

--- src/event/BlazeBroker.ts
+++ src/event/BlazeBroker.ts
@@ -1,25 +1,25 @@
 import { type ActionCallResult as Result } from '@/types/action';
-import { type EventArgs, type EventName } from '@/types/event';
+import { type EventName } from '@/types/event';
 import { BlazeEvent } from './BlazeEvent';
 
 export class BlazeBroker {
   // eslint-disable-next-line no-use-before-define, @typescript-eslint/no-shadow
   public async call<T, U = T extends Array<infer T> ? Result<T> : Result<T>>(
     eventName: EventName,
-    params: EventArgs
+    ...values: unknown[]
   ) {
-    return BlazeEvent.emitAsync<T, U>(eventName, params);
+    return BlazeEvent.emitAsync<T, U>(eventName, ...values);
   }
 
   // eslint-disable-next-line no-use-before-define, @typescript-eslint/no-shadow
   public async mcall<T, U = T extends Array<infer T> ? Result<T> : Result<T>>(
-    ...args: [eventName: EventName, params: EventArgs][]
+    ...args: [eventName: EventName, ...values: unknown[]][]
   ) {
-    return Promise.all(args.map((a) => this.call<T, U>(...a)));
+    return Promise.all(args.map((values) => this.call<T, U>(...values)));
   }
 
-  public emit(eventName: EventName, params: EventArgs) {
-    return BlazeEvent.emit(eventName, params);
+  public emit(eventName: EventName, ...values: unknown[]) {
+    return BlazeEvent.emit(eventName, ...values);
   }
 
   public eventNames() {
