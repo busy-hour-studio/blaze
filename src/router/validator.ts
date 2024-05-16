a4d7c081ed732cfc9ea7fb8aa5fa57b3e6540d4d
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-05-16 16:42:48 +0700

    fix: openapi error

--- src/router/validator.ts
+++ src/router/validator.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { BlazeDependency } from '../config';
 import { Logger } from '../errors/Logger';
+import { Random } from '../types/helper';
 import { ExternalModule } from '../utils/constant';
 
 const zodApi = BlazeDependency.modules[ExternalModule.ZodApi];
@@ -8,8 +9,12 @@ const zodApi = BlazeDependency.modules[ExternalModule.ZodApi];
 if (zodApi) {
   zodApi.extendZodWithOpenApi(z);
 } else {
-  z.ZodType.prototype.openapi = () => {
+  z.ZodType.prototype.openapi = function openapi() {
     Logger.warn(`Please install "${ExternalModule.ZodApi}" to use OpenAPI.`);
+
+    const result = new (this as Random).constructor(this._def);
+
+    return result;
   };
 }
 
